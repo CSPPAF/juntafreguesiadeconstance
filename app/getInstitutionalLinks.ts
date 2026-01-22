6211864dca3b3c1c7d67c70dff0cb54266a144b6
CSPPAF <qualidade@centropadreangelo.pt>
2026-01-22 14:07:00 +0000

    Update getInstitutionalLinks.ts

--- app/getInstitutionalLinks.ts
+++ app/getInstitutionalLinks.ts
@@ -1,19 +1,31 @@
 import { cache } from 'react'
 import { sanityClient } from '../sanityClient'
 
+// Define manualmente o tipo de imagem do Sanity
+export type SanityImage = {
+  _type: 'image'
+  asset: {
+    _ref: string
+    _type: 'reference'
+  }
+}
+
 export type SanityInstitutionalLink = {
   title: string
   url: string
   order?: number
-  logo: any
+  logo: SanityImage
 }
 
-export const getInstitutionalLinks = cache(async (): Promise<SanityInstitutionalLink[]> => {
-  return sanityClient.fetch(`
-    *[_type == "institutionalLink"] | order(order asc) {
-      title,
-      url,
-      logo
-    }
-  `)
-})
+export const getInstitutionalLinks = cache(
+  async (): Promise<SanityInstitutionalLink[]> => {
+    return sanityClient.fetch(`
+      *[_type == "institutionalLink"] | order(order asc) {
+        title,
+        url,
+        order,
+        logo
+      }
+    `)
+  }
+)
